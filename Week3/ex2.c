64f248728c27f51d5941aabbf7a0f97ba597f3b4
Talaxika <81813961+Talaxika@users.noreply.github.com>
2022-04-04 10:46:01 +0300

    Create ex2.c

---
+++ Week3/ex2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <math.h>
+
+//run command -gcc ex2.c -o ex2 - ./ex2 myfile.txt output.hex -TB
+
+int main(int argc, char* argv[]) {
+    FILE* fpr = fopen(argv[1], "r");
+    FILE* fpwr = fopen(argv[2], "wb");
+
+    if (NULL == fpr || NULL == fpwr)
+    {
+        perror("Error info");
+        return 1;
+    }
+
+    char buffer[200];
+    char upper[200] = "65536";
+    while ((fgets(buffer, 200, fpr)) != NULL)
+    {
+        int result = strcmp(buffer, upper);
+        if (buffer < 0 || result > 0)
+        {
+            fprintf(stderr, "Wrong input.");
+        }
+               
+        fputs(buffer, fpwr);
+    }
+    
+
+    if(fclose(fpr) == -1 || fclose(fpwr) == -1){
+        fprintf(stderr, "File problem\n");
+        return 1;
+    }
+
+    return 0;
+}
